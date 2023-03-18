b25991e3d9de2a73d737c7ebe46d8257be719dbd
hoangphan636 <57717259+hoangphan636@users.noreply.github.com>
2023-03-18 10:28:05 +0700

    add confirm delete
    add confirm delete

--- Hotel_Management_Web/Pages/Staffs/Index.cshtml.cs
+++ Hotel_Management_Web/Pages/Staffs/Index.cshtml.cs
@@ -15,16 +15,34 @@ namespace Hotel_Management_Web.Pages.Staffs
     {
         private readonly IStaffRepository staf = new StaffRepository();
         private readonly ICustomerRepository cus = new CustomerRepository();
-
+       
+        public string Role { get; set; }
         public List<staff> staff { get;set; }
 
         public IActionResult OnGet()
         {
+
             string cont = HttpContext.Session.GetString("username");
-         
-            staff = staf.Searchingstaff(cont);
-            return Page();
-           
+            string Role = HttpContext.Session.GetString("role");
+
+            if (HttpContext.Session.GetString("username") == null)
+            {
+                return RedirectToPage("/Welcome");
+            }
+            else if (!Role.Equals("2"))
+            {
+                return RedirectToPage("/Errors");
+            }
+            else if (Role.Equals("2"))
+            {
+                staff = staf.Searchingstaff(cont);
+                return Page();
+            }else if(Role.Equals("1"))
+            {
+                staff = staf.GetAllstaff();
+                return Page();
+            }
+            return RedirectToPage("/Errors");
         }
     
             public IActionResult OnGetLog()
